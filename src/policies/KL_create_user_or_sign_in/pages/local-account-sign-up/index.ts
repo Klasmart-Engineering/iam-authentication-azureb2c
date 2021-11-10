790dfdd458a9138658050508d16f3936cfff2484
Max Nicholson <max.nicholson@calmid.com>
2021-11-10 11:54:07 +0000

    feat: add privacy policy hyperlink to label

---
+++ src/policies/KL_create_user_or_sign_in/pages/local-account-sign-up/index.ts
@@ -0,0 +1,51 @@
+import DOMPurify from "dompurify"
+
+const PRIVACY_POLICY_ID = "extension_HasAcceptedPrivacyPolicy_true"
+const PRIVACY_POLICY_URL =
+    "https://klukb2cstorage.blob.core.windows.net/b2ccosmosdb/privacy-policy/index.html"
+const DEFAULT_PRIVACY_POLICY_TEXT = "I agree to the KidsLoop {{Privacy policy}}"
+const PLACEHOLDER_REGEX = /{{(.+)}}/
+
+// Adapted from https://docs.microsoft.com/en-us/azure/active-directory-b2c/javascript-and-page-layout?pivots=b2c-custom-policy#add-terms-of-use
+function addPrivacyPolicyLink() {
+    const label = document.querySelector(
+        `#api label[for="${PRIVACY_POLICY_ID}"]`
+    )
+    if (!label) {
+        console.error("Privacy Policy input not found")
+        return
+    }
+
+    const labelText = label.textContent || DEFAULT_PRIVACY_POLICY_TEXT
+
+    const placeholderMatches = labelText.match(PLACEHOLDER_REGEX)
+
+    const anchor = document.createElement("a")
+    anchor.setAttribute("href", PRIVACY_POLICY_URL)
+    anchor.setAttribute("target", "_blank")
+    anchor.setAttribute("rel", "noopener noreferrer")
+
+    let rawHTML: string
+    if (placeholderMatches !== null) {
+        // Replace the placeholder with the link
+        anchor.textContent = placeholderMatches[1]
+        rawHTML = labelText.replace(PLACEHOLDER_REGEX, anchor.outerHTML)
+    } else {
+        // Make the entire label a link
+        anchor.textContent = labelText
+        rawHTML = anchor.outerHTML
+    }
+
+    // May not strictly be necessary, as we should control the initial text and the link
+    // but can't hurt
+    const cleanHTML = DOMPurify.sanitize(rawHTML, {
+        ALLOWED_ATTR: ["target", "rel", "href"],
+    })
+    label.innerHTML = cleanHTML
+}
+
+if (document.readyState !== "loading") {
+    addPrivacyPolicyLink()
+} else {
+    document.addEventListener("DOMContentLoaded", addPrivacyPolicyLink)
+}

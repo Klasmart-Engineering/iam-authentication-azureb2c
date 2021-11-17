21130409c414df61bebdc3d0d7f47f2b5eb225f9
Max Nicholson <max.nicholson@calmid.com>
2021-11-17 17:39:31 +0000

    build(webpack): relative CSS/asset links in HTML

--- webpack.config.js
+++ webpack.config.js
@@ -12,26 +12,32 @@ const glob = require("fast-glob")
  */
 function buildEntrypoints() {
     return Object.fromEntries(
-        glob.sync("src/**/index.ts").map((file) => {
-            const pathInfo = path.parse(file)
-            // Strip `src` from start of folder path
-            const [_, ...relativePath] = pathInfo.dir.split(path.sep)
-            return [
-                // e.g. path/to/file/index
-                path.join(...relativePath, pathInfo.name),
-                // e.g. ./<repo_root>/src/path/to/file/index.ts
-                path.resolve(file),
-            ]
-        })
+        glob
+            .sync("src/**/index.html")
+            .concat(glob.sync("src/**/bundle.ts"))
+            .map((file) => {
+                const pathInfo = path.parse(file)
+                // Strip `src` from start of folder path
+                const [_, ...relativePath] = pathInfo.dir.split(path.sep)
+                return [
+                    // e.g. path/to/file/index
+                    path.join(...relativePath, pathInfo.name),
+                    // e.g. ./<repo_root>/src/path/to/file/index.ts
+                    path.resolve(file),
+                ]
+            })
     )
 }
 
 module.exports = {
     mode: "production",
     entry: buildEntrypoints(),
+    context: path.resolve(__dirname, "src"),
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
+        publicPath: "https://klukb2cstorage.blob.core.windows.net/b2ccosmosdb/",
+        assetModuleFilename: "[name][ext]",
     },
     module: {
         rules: [
@@ -41,6 +47,47 @@ module.exports = {
                 use: {
                     loader: `babel-loader`,
                 },
+            },
+            {
+                test: /\.svg$/,
+                use: {
+                    // Ideally we would use `type: "asset/resource" here, but it doesn't work with `extract-loader`
+                    loader: "file-loader",
+                    options: { name: "[path][name].[ext]" },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        // Ideally we would use `type: "asset/resource" here, but it doesn't work with `extract-loader`
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name].[ext]",
+                        },
+                    },
+                    "extract-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "[path][name][ext]",
+                },
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    "extract-loader",
+                    { loader: "html-loader", options: { esModule: false } },
+                ],
             },
         ],
     },

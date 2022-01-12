80d6e116f22f478ef56f367da5c2ac63ddb2c9b1
Rosu Mihai <MihaiAlexandru.Rosu@endava.com>
2022-01-12 12:41:28 +0200

    ran prettier to format code

--- src/types/translationsTypes.ts
+++ src/types/translationsTypes.ts
@@ -1,58 +1,36 @@
-
 export interface Data {
-
     TrustFrameworkPolicy: TrustFrameworkPolicy
-
 }
 
 export interface TrustFrameworkPolicy {
-
     BuildingBlocks: BuildingBlocks
-
 }
-
 
 export interface BuildingBlocks {
-
     Localization: Localization
-
 }
-
 
 export interface Localization {
-
     LocalizedResources: LocalizedResource[]
-
 }
 
-
 export interface LocalizedResource {
-
     Id: string
 
     LocalizedStrings: LocalizedStrings
 
     LocalizedCollections?: LocalizedCollections
-
 }
-
 
 export interface LocalizedCollections {
-
     LocalizedCollection: LocalizedCollection | LocalizedCollection[]
-
 }
-
 
 export interface LocalizedStrings {
-
     LocalizedString: LocalizedString[]
-
 }
 
-
 export interface LocalizedString {
-
     ElementType: string
 
     ElementId?: string | null
@@ -60,12 +38,9 @@ export interface LocalizedString {
     StringId: string
 
     $t: string
-
 }
 
-
 export interface LocalizedCollection {
-
     ElementType: string
 
     ElementId: string
@@ -73,13 +48,10 @@ export interface LocalizedCollection {
     TargetCollection: string
 
     Item: Item
-
 }
 
 export interface Item {
-
     Text: string
 
     Value: string
-
 }

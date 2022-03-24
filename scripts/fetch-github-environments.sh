b457a16ded9fe523bcb656b268ee58f7088264f4
Max Nicholson <max.nicholson@kidsloop.live>
2022-03-24 13:39:05 +0000

    ci(deploy): output environments on a single line
    Required for set-output to work correcly

--- scripts/fetch-github-environments.sh
+++ scripts/fetch-github-environments.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# NB: Must ensure jq output is on a single line (-c) as Github workflow `set-output` doesn't support
+# multiline strings
+
 curl \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer $1" \
     --fail \
-    https://api.github.com/repos/KL-Engineering/iam-authentication-azureb2c/environments | jq '.environments | map(.name)'
+    https://api.github.com/repos/KL-Engineering/iam-authentication-azureb2c/environments | jq -c '.environments | map(.name)'

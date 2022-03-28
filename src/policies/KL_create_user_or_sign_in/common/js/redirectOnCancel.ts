4f1d9b4a32fd30f86386bc38c638b25271c7af93
Hamza Mihai Daniel <mihaidaniel.hamza@endava.com>
2022-03-28 11:58:04 +0300

    added preventDefault

--- src/policies/KL_create_user_or_sign_in/common/js/redirectOnCancel.ts
+++ src/policies/KL_create_user_or_sign_in/common/js/redirectOnCancel.ts
@@ -4,6 +4,7 @@ export const setupRedirectOnCancel = () => {
     document
         .getElementById(BUTTON_CANCEL_ID)
         ?.addEventListener("click", (event: MouseEvent) => {
+            event.preventDefault()
             event.stopImmediatePropagation()
 
             window.history.back()

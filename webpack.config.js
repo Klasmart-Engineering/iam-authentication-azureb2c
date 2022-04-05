b29fe2efac33cb428149b263d29f7bfc13c1e1d4
Hamza Mihai Daniel <mihaidaniel.hamza@endava.com>
2022-04-05 18:41:46 +0300

    update the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -103,6 +103,9 @@ module.exports = ({ environment }) => {
                     use: [
                         {
                             loader: "file-loader",
+                            options: {
+                                name: "[path][name].[ext]",
+                            },
                         },
                     ],
                 },
